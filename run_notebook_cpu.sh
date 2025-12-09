bd581023e7b141563ddfece5a601bb511ebf3c79
kkazuma19 <kazumak2@illinois.edu>
2025-12-08 20:38:12 -0600

    add logs

--- run_notebook_cpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Generate a random port number between 49152 and 59151
-MYPORT=$(($(($RANDOM % 10000))+49152))
-
-# Echo the port number for the user to see
-echo "Using port: $MYPORT"
-
-# Run the jupyter-notebook command with the specified parameters
-srun --account=bcnx-delta-cpu --partition=cpu --time=06:00:00 --nodes=1 --ntasks-per-node=1 --mem=120g jupyter-notebook --no-browser --port=$MYPORT --ip=0.0.0.0
